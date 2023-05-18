19fbc53efdf255fabb6ad01b9dc841217e0b7af6
Siddeshwar7 <106647336+Siddeshwar7@users.noreply.github.com>
2023-05-18 15:20:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@ import streamlit as st
 import subprocess
 from PIL import Image
 
-st.subheader('1. Printing text in R')
-with st.expander('See code'):
-  code1 = '''print("Hello world ...")
-  '''
-  st.code(code1, language='R')
 process1 = subprocess.Popen(["Rscript", "Audit_sampling.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 result1 = process1.communicate()
 st.write(result1)
